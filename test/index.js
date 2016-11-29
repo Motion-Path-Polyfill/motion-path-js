273bc7df38ba96d0be173dac4febd9d3b28d0c19
Divyanshi Vashist <divyanshi@google.com>
2016-11-29 11:12:04 +1100

    Added more npm commands

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-suite("index", function() {
-	test("hello", function() {
-		chai.assert.equal(hello("a"), "Hello a!");
-		chai.assert.equal(hello("b"), "Hello b!");
-	}) 
-})
+/* global suite test chai hello */
+suite('index', function () {
+  test('hello', function () {
+    chai.assert.equal(hello('a'), 'Hello a!');
+    chai.assert.equal(hello('b'), 'Hello b!');
+  });
+});
