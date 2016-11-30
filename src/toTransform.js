f19c9d1f2b2e13a88e2204a533f517e8d555c691
Divyanshi Vashist <divyanshi@google.com>
2016-11-30 16:37:16 +1100

    Fixed the style

--- src/toTransform.js
+++ src/toTransform.js
@@ -1,134 +1,127 @@
-(function() {
-	window.InvalidArgument = function InvalidArgument(message) {
-		this.message = message;
-		this.name = "InvalidArgument";
-	} 
+(function () {
+  function InvalidArgument (message) {
+    this.message = message;
+    this.name = 'InvalidArgument';
+  }
 
-	function checkTranslate(properties) {
-		var result = '';
-		if (properties.translate != undefined && properties.translate != 'none') {
-			
-			result += 'translate(';
+  function checkTranslate (properties) {
+    var result = '';
+    if (properties.translate !== undefined && properties.translate !== 'none') {
+      result += 'translate(';
 
-			var values = properties.translate;
-			var valuesArray = values.split(" ");
-			var numValues = valuesArray.length;
+      var values = properties.translate;
+      var valuesArray = values.split(' ');
+      var numValues = valuesArray.length;
 
+      if (numValues > 3) {
+        throw new InvalidArgument('Too many values for translate');
+      }
 
-			if (numValues > 3) {
-				throw new InvalidArgument("Too many values for translate");
-			}
+      if (numValues === 1 && valuesArray[0] === '') {
+        throw new InvalidArgument('Empty string is not a valid argument');
+      }
 
-			if (numValues == 1 && valuesArray[0] == '') {
-				throw new InvalidArgument("Empty string is not a valid argument");
-			}
-			
-			for (var counter=0; counter < numValues - 1; counter++) {
-				result += valuesArray[counter] + ', ';
-			}
+      for (var counter = 0; counter < numValues - 1; counter++) {
+        result += valuesArray[counter] + ', ';
+      }
 
-			result += valuesArray[counter] + ')';
-		}
-		return result;
-	} 
+      result += valuesArray[counter] + ')';
+    }
+    return result;
+  }
 
-	function checkRotate(properties) {
-		var result = '';
-		if (properties.rotate != undefined && properties.rotate != 'none') {
-			if(properties.translate != undefined && properties.translate != 'none') {
-				result += ' ';
-			}
-			result += 'rotate(';
+  function checkRotate (properties) {
+    var result = '';
+    if (properties.rotate !== undefined && properties.rotate !== 'none') {
+      if (properties.translate !== undefined && properties.translate !== 'none') {
+        result += ' ';
+      }
+      result += 'rotate(';
 
-			values = properties.rotate;
-			valuesArray = values.split(" ");
-			numValues = valuesArray.length;
+      var values = properties.rotate;
+      var valuesArray = values.split(' ');
+      var numValues = valuesArray.length;
 
-			if (numValues != 4 && numValues != 1) {
-				throw new InvalidArgument("Incorrect number of arguments for rotate");
-			}
+      if (numValues !== 4 && numValues !== 1) {
+        throw new InvalidArgument('Incorrect number of arguments for rotate');
+      }
 
-			if (numValues == 1 && valuesArray[0] == '') {
-				throw new InvalidArgument("Empty string is not a valid argument");
-			}
+      if (numValues === 1 && valuesArray[0] === '') {
+        throw new InvalidArgument('Empty string is not a valid argument');
+      }
 
-			// doesn't check ordering of arguments
-			var angleUnits = ['deg', 'grad', 'rad', 'turn'];
-			var angleUnit = valuesArray[numValues-1];
+      var angleUnits = ['deg', 'grad', 'rad', 'turn'];
+      var angleUnit = valuesArray[numValues - 1];
 
-			var foundUnit = 0;
-			var unit = angleUnit.substring(angleUnit.length - 3, angleUnit.length);
+      var foundUnit = 0;
+      var unit = angleUnit.substring(angleUnit.length - 3, angleUnit.length);
 
-			if (angleUnits.indexOf(unit) != -1) {
-				foundUnit = 1;
-			}
+      if (angleUnits.indexOf(unit) !== -1) {
+        foundUnit = 1;
+      }
 
-			if (foundUnit == 0) {
-				var unit = angleUnit.substring(angleUnit.length - 4, angleUnit.length);
-				if (angleUnits.indexOf(unit) != -1) {
-					foundUnit = 1;
-				}
-			}
-		
+      if (foundUnit === 0) {
+        unit = angleUnit.substring(angleUnit.length - 4, angleUnit.length);
+        if (angleUnits.indexOf(unit) !== -1) {
+          foundUnit = 1;
+        }
+      }
 
-			if (foundUnit == 0) {
-				throw new InvalidArgument('Incorrect angle units');
-			}
+      if (foundUnit === 0) {
+        throw new InvalidArgument('Incorrect angle units');
+      }
 
-			for (counter=0; counter < numValues - 1; counter++) {
-				result += valuesArray[counter] + ' ';
-			}
-			
-			result += valuesArray[counter] + ')';
-		}
-		return result;
-	} 
+      for (var counter = 0; counter < numValues - 1; counter++) {
+        result += valuesArray[counter] + ' ';
+      }
 
-	function checkScale(properties) {
-		var result = '';
-		if (properties.scale != undefined && properties.scale != 'none') {
-			if(properties.translate != undefined && properties.translate != 'none') {
-				result += ' ';
-			} else if(properties.rotate != undefined && properties.rotate != 'none') {
-				result += ' ';
-			}
+      result += valuesArray[counter] + ')';
+    }
+    return result;
+  }
 
-			result += 'scale(';
+  function checkScale (properties) {
+    var result = '';
+    if (properties.scale !== undefined && properties.scale !== 'none') {
+      if (properties.translate !== undefined && properties.translate !== 'none') {
+        result += ' ';
+      } else if (properties.rotate !== undefined && properties.rotate !== 'none') {
+        result += ' ';
+      }
 
-			var values = properties.scale;
-			var valuesArray = values.split(" ");
-			var numValues = valuesArray.length;
+      result += 'scale(';
 
+      var values = properties.scale;
+      var valuesArray = values.split(' ');
+      var numValues = valuesArray.length;
 
-			if (numValues < 1 && numValues > 3) {
-				throw new InvalidArgument("Incorrect number of values for scale");
-			} 
+      if (numValues < 1 && numValues > 3) {
+        throw new InvalidArgument('Incorrect number of values for scale');
+      }
 
-			if (numValues == 1 && valuesArray[0] == '') {
-				throw new InvalidArgument("Empty string is not a valid argument");
-			}
-			
-			for (var counter=0; counter < numValues - 1; counter++) {
-				result += valuesArray[counter] + ' ';
-			}
+      if (numValues === 1 && valuesArray[0] === '') {
+        throw new InvalidArgument('Empty string is not a valid argument');
+      }
 
-			result += valuesArray[counter] + ')';
-		}
-			
-		return result;
-	} 
+      for (var counter = 0; counter < numValues - 1; counter++) {
+        result += valuesArray[counter] + ' ';
+      }
 
-	window.toTransform = function(properties) {
-		var result = '';
+      result += valuesArray[counter] + ')';
+    }
 
-		result += checkTranslate(properties); 
-		result += checkRotate(properties);
-		result += checkScale(properties);
+    return result;
+  }
 
-		return result;
-	}
+  window.toTransform = function (properties) {
+    var result = '';
 
-	//var thing = toTransform({translate: '20px 30px', rotate: '10 10 30 40turn', scale: '2 3'});
+    result += checkTranslate(properties);
+    result += checkRotate(properties);
+    result += checkScale(properties);
 
-	//console.log(thing);
+    return result;
+  };
+
+  window.InvalidArgument = InvalidArgument;
 })();
--- test/toTransform.js
+++ test/toTransform.js
@@ -1,19 +1,17 @@
-/* global suite test chai toTransform */
+/* global suite test assert toTransform InvalidArgument */
 
 suite('toTransform', function () {
   test('testTranslate', function () {
-  	
-  	assert.throws(function() {
-  		toTransform = toTransform({translate: ''});
-  	}, InvalidArgument);
+    assert.throws(function () {
+      toTransform({translate: ''});
+    }, InvalidArgument);
 
-  	assert.equal(toTransform({translate: 'none'}), '');
-  	
-  	assert.throws(function() {
-  		toTransform = toTransform({translate: '20px 30px 27px 90px'});
-  	}, InvalidArgument);
+    assert.equal(toTransform({translate: 'none'}), '');
 
-    chai.assert.equal(toTransform({translate: '20px 30px'}), 'translate(20px, 30px)');
- 
+    assert.throws(function () {
+      toTransform({translate: '20px 30px 27px 90px'});
+    }, InvalidArgument);
+
+    assert.equal(toTransform({translate: '20px 30px'}), 'translate(20px, 30px)');
   });
 });
