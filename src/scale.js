5762f5d434388567fbd355275d38f34de26bcec2
Courtney Bracefield <cbracefield@google.com>
2016-12-14 11:08:02 +1100

    making changes according to review comments

--- src/scale.js
+++ src/scale.js
@@ -1,20 +1,13 @@
-/* global WebAnimationsPolyfillExtension internalScope */
+/* global WebAnimationsPolyfillExtension internalScope*/
 
 (function () {
+
   function isNumeric (number) {
     return !isNaN(number);
   }
 
-  function addDefaultAxis (values) {
-    if (values.length === 1) { // x-axis was specified
-      values.push(1, 1);
-    } else if (values.length === 2) { // x and y-axis were specified
-      values.push(1);
-    }
-    return values;
-  }
-
   function scaleParse (input) {
+    var InvalidArgument = internalScope.InvalidArgument; 
     /* According to spec:
       https://drafts.csswg.org/css-transforms-2/#propdef-scale
       unspecified scales default to 1
@@ -28,24 +21,20 @@
     var values = input.split(/\s+/);
     var numValues = values.length;
     if (numValues < 1 || numValues > 3) {
-      return undefined;
+      throw new InvalidArgument('Incorrect number of values for scale');
     }
 
     for (var i = 0; i < numValues; i++) {
-      if (values[i] === '') {
-        return undefined;
-      }
-
-      if (!isNumeric(values[i])) {
-        return undefined;
+      if (values[i] === '' || !isNumeric(values[i])) {
+        throw new InvalidArgument('Argument must be a number');
       }
     }
 
-    for (var j = 0; j < numValues; j++) {
-      values[j] = Number(values[j]);
-    }
+    values = values.map(Number);
 
-    values = addDefaultAxis(values);
+    while (values.length < 3) {
+      values.push(1);
+    }
 
     return values;
   }
@@ -60,18 +49,39 @@
     };
   }
 
+  function parseManager(input) {
+    try {
+      return scaleParse(input);
+    } catch (error) {
+      if (error.constructor === InvalidArgument) {
+        return undefined;
+      }
+    }
+  }
+
   WebAnimationsPolyfillExtension.register({
     name: 'scale',
     properties: {
-      scale: {
-        parse: scaleParse,
+       scale: {
+        parse: parseManager,
         merge: merge
       }
     },
     applyHook: {
       callback: function (values) {
         var toTransform = internalScope.toTransform;
+        //var scale = values.scale;
         var transformString = toTransform(values);
+        /*if (scale === undefined) {
+          return null;
+        } else if (scale === 'none') {
+          //return {transform: 'scale(1, 1, 1)' + values.transform};
+          tranformSr
+        }*/
+
+        //var valuesArray = values.scale.split(/\s+/);
+        //var scaleStr = 'scale3d(' + valuesArray.join(', ') + ')';
+
         return {transform: transformString + ' ' + values.transform};
       },
       watchedProperties: ['scale', 'transform']
