98a750938129610a38a2ebd679a2b31eaf88e16f
Courtney Bracefield <cbracefield@google.com>
2016-12-19 16:37:45 +1100

    removed functions in rotateTest.js because they are now in their own file.

--- test/rotateTest.js
+++ test/rotateTest.js
@@ -1,54 +1,11 @@
-/* global suite test assert */
+/* global suite test internalScope */
 
 (function () {
-  var InvalidTransformValue = {};
-
-  function checkTransformKeyframes (keyframes) {
-    var target = document.createElement('div');
-
-    for (var value of keyframes) {
-      target.style.transform = '';
-      target.style.transform = value;
-      if (value === InvalidTransformValue) {
-        continue;
-      }
-      assert.notEqual(target.style.transform, '',
-        'Invalid expected keyframe: ' + value);
-    }
-  }
-
-  function isAnimationEqual (actualKeyframes, expectedKeyframes) {
-    var timing = {duration: 1};
-    var currentTimes = [0.10, 0.25, 0.50, 0.75, 0.90];
-
-    checkTransformKeyframes(expectedKeyframes.transform);
-
-    for (var currentTime of currentTimes) {
-      var actualTarget = document.createElement('div');
-      document.body.appendChild(actualTarget);
-
-      var expectedTarget = document.createElement('div');
-      document.body.appendChild(expectedTarget);
-
-      var animation = actualTarget.animate(actualKeyframes, timing);
-      animation.currentTime = currentTime;
-      var result = window.getComputedStyle(actualTarget).transform;
-      animation.cancel();
-
-      animation = expectedTarget.animate(expectedKeyframes, timing);
-      animation.currentTime = currentTime;
-      var expected = window.getComputedStyle(expectedTarget).transform;
-      animation.cancel();
-
-      actualTarget.remove();
-      expectedTarget.remove();
-
-      assert.equal(result, expected, 'at currentTime ' + currentTime + ' comparing ' + JSON.stringify(actualKeyframes) + ' with ' + JSON.stringify(expectedKeyframes));
-    }
-  }
-
   suite('transforms', function () {
     test('rotateTransform', function () {
+      var isAnimationEqual = internalScope.isAnimationEqual;
+      var InvalidTransformValue = internalScope.InvalidTransformValue;
+
       isAnimationEqual({rotate: ['100grad', '200grad']}, {transform: ['rotate(100grad)', 'rotate(200grad)']});
 
       // Web animations transform does not support turn, but it should!
