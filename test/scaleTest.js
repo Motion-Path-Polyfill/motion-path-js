83fc16f67098ede29e280702252154dfc9eb0adb
Courtney Bracefield <cbracefield@google.com>
2016-12-19 16:09:29 +1100

    re-ordering

--- test/scaleTest.js
+++ test/scaleTest.js
@@ -1,11 +1,11 @@
 /* global suite test internalScope */
 
 (function () {
-  var isAnimationEqual = internalScope.isAnimationEqual;
-  var InvalidTransformValue = internalScope.InvalidTransformValue;
-
   suite('transforms', function () {
     test('scaleTransform', function () {
+      var isAnimationEqual = internalScope.isAnimationEqual;
+      var InvalidTransformValue = internalScope.InvalidTransformValue;
+
       isAnimationEqual({scale: ['0.5', '2.5']}, {transform: ['scale3d(0.5, 1, 1)', 'scale3d(2.5, 1, 1)']});
       isAnimationEqual({scale: ['9 2', '2 2']}, {transform: ['scale3d(9, 2, 1)', 'scale3d(2, 2, 1)']});
       isAnimationEqual({scale: ['1 2 3', '3 4 5']}, {transform: ['scale3d(1, 2, 3)', 'scale3d(3, 4, 5)']});
