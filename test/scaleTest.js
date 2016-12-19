3c3ba4187a8010ca1dad3f439c712f4210c9dd28
Courtney Bracefield <cbracefield@google.com>
2016-12-19 15:33:24 +1100

    removed test functions from scaleTest.js as they are now in their own file.

--- test/scaleTest.js
+++ test/scaleTest.js
@@ -1,50 +1,8 @@
-/* global suite test assert */
+/* global suite test assert internalScope */
 
 (function () {
-  var InvalidTransformValue = {};
-
-  function checkTransformKeyframes (keyframes) {
-    var target = document.createElement('div');
-
-    for (var value of keyframes) {
-      target.style.transform = '';
-      target.style.transform = value;
-      if (value === InvalidTransformValue) {
-        continue;
-      }
-
-      assert.notEqual(target.style.transform, '',
-        'Invalid expected keyframe: ' + value);
-    }
-  }
-
-  function isAnimationEqual (actualKeyframes, expectedKeyframes) {
-    var timing = {duration: 1};
-    var currentTimes = [0.10, 0.25, 0.50, 0.75, 0.90];
-
-    checkTransformKeyframes(expectedKeyframes.transform);
-
-    for (var currentTime of currentTimes) {
-      var actualTarget = document.createElement('div');
-      document.body.appendChild(actualTarget);
-
-      var expectedTarget = document.createElement('div');
-      document.body.appendChild(expectedTarget);
-
-      var animation = actualTarget.animate(actualKeyframes, timing);
-      animation.currentTime = currentTime;
-      var result = window.getComputedStyle(actualTarget).transform;
-
-      animation = expectedTarget.animate(expectedKeyframes, timing);
-      animation.currentTime = currentTime;
-      var expected = window.getComputedStyle(expectedTarget).transform;
-
-      actualTarget.remove();
-      expectedTarget.remove();
-
-      assert.equal(result, expected, 'at currentTime ' + currentTime + ' comparing ' + JSON.stringify(actualKeyframes) + ' with ' + JSON.stringify(expectedKeyframes));
-    }
-  }
+  var isAnimationEqual = internalScope.isAnimationEqual;
+  var InvalidTransformValue = internalScope.InvalidTransformValue;
 
   suite('transforms', function () {
     test('scaleTransform', function () {
